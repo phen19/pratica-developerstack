e4d29c3614d1311777c979c3093b598135107466
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-09-08 20:44:00 -0300

    fix: adjustment validation

--- src/services/answerService.ts
+++ src/services/answerService.ts
@@ -1,10 +1,13 @@
 // TODO
 import { Answer } from "@prisma/client";
 import * as answerRepository from "./../repositories/answerRepository";
+import * as questionRepository from "./../repositories/questionRepository";
 
 export type CreateAnswerData = Answer;
 
 async function insert(createAnswerData: CreateAnswerData) {
+  const question = await questionRepository.findById(createAnswerData.questionId);
+  if (!question) throw { type: "not_found" };
   await answerRepository.insert(createAnswerData);
 }
 
